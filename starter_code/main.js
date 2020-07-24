27d8abf5bd91e1a3bfd2a47455e4bd304bfa9fe9
Otis Guess <guess01325@gmail.com>
2020-07-24 02:30:29 -0400

    [functions] worked on functions deliverable.

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,35 +1,58 @@
 // Question 1
 function maxOfTwoNumbers() {
-
+    return Math.max.apply(null,array);
 }
+var biggerOfTwo=[25, 26];
+var biggerOfTwo=getArrayMax(biggerOfTwo)
+console.log(biggerOfTwo);
 
 // Question 2
 function maxOfThree() {
-
+    return Math.max.apply(null,array);
 }
+var three=[23, 25, 27];
+var three=getArrayMax(three)
+console.log(three);
 
 // Question 3
 function isCharacterAVowel() {
-
-}
+    const vowels = ["a", "e", "i", "o", "u"];
+    if (vowels.includes(char)) {
+      return true;
+    } else {
+        return false;
+      }
+    }
+    console.log(" Is this character a vowel ? " + isCharacterAVowel("y"));
 
 // Question 4
 function sumArray() {
- 
+    let mutiply = 1;
+    for (let i = 0; i < anArray.length; i++) {
+        mutiply *= anArray[i];
+      }
+    
+      return mutiply;
 }
+console.log(multiplyArray([1, 2, 3, 4]));
 
 
 // Question 4
 function multiplyArray() {
-
+    let multiply = 1; 
+    for (var i = 0; i < someArray.length; i++){
+        multiply *= someArray[i];
+    }
+    return multiply;
 }
+console.log(multiplyArray([1, 2, 3, 4]))
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+    console.log(multiplyArray([1, 2, 3, 4]))
 }
-
+console.log(numberOfArguments(23, 25, 26, 25, 22, 'sandwich', 23));
 
 
 // Question 6
